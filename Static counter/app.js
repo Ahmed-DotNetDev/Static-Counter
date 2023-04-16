72cb393156f22e46787a4f76d632453a5dc462f7
Ahmed.net <Ahmed.net@gmail.com>
2023-04-16 04:06:44 +0200

    Modify files

--- Static counter/app.js
+++ Static counter/app.js
@@ -1,4 +1,21 @@
+let count = 0; 
+
+document.getElementById("increase").onclick = function () {
+    count++;
+    document.getElementById("countlbl").innerHTML = count;
+}
+
+document.getElementById("reset").onclick = function () {
+    count = 0;
+    document.getElementById("countlbl").innerHTML = count;
+}
+
+document.getElementById("decrease").onclick = function () {
+    count--;
+    document.getElementById("countlbl").innerHTML = count;  
+}
 
 
 
-let demo=document.getElementById("demo").innerHTML=maximum;
+
+
